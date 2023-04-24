6b6d51e56cf5975e23de5ea75e2c9acf4d97f048
idkneel <idkneel@gmail.com>
2023-04-24 15:48:27 -0400

    more fixes

--- app/src/main/java/com/example/qalendar/Events.java
+++ app/src/main/java/com/example/qalendar/Events.java
@@ -5,6 +5,7 @@ import android.view.View;
 import androidx.annotation.NonNull;
 
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
