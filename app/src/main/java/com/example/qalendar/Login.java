e043fa5d7e7084423be8b65092bcaa0ca3b997aa
jhgugvut <Japhet8marvel@gmail.com>
2023-04-26 20:30:29 -0400

    Committing just so I don't lose any progress.

--- app/src/main/java/com/example/qalendar/Login.java
+++ app/src/main/java/com/example/qalendar/Login.java
@@ -5,10 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Login extends AppCompatActivity {
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    public void onCreate(@Nullable Bundle savedInstanceState,
+                         @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                // .setPasswordRequestOptions(PasswordRequestOptions.builder()
+                //         .setSupported(true)
+                //         .build())
+                .setGoogleIdTokenRequestOptions(GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))   // qalendar-6e694
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build();
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+        }
     }
+
 }
