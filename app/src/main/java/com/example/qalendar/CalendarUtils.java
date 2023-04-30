e96b69d3077fd42d0b366790bd9e1fd431b96fb8
Jack Komperda <JKomperda@student.fullsail.edu>
2023-04-30 15:56:04 -0400

    Debugged CalendarUtils.java Notes:
    Missing opening bracket for monthDayFromDate method.
    sundayForDate and monthYearFromDate were both defined twice?
    Missing opening bracket for daysInWeekArray method.
    
    Some other alt-enter fixes I don't recall off the top of my head to add variable definitions.

--- app/src/main/java/com/example/qalendar/CalendarUtils.java
+++ app/src/main/java/com/example/qalendar/CalendarUtils.java
@@ -7,44 +7,35 @@ import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-public class CalendarUtils
-{
+public class CalendarUtils {
     public static LocalDate selectedDate = LocalDate.now();
 
-    public static String formattedDate(LocalDate selectedDate)
-    {
+    public static String formattedDate(LocalDate selectedDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
         return selectedDate.format(formatter);
     }
-    public static String formattedTime(LocalTime time)
-    {
+
+    public static String formattedTime(LocalTime time) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
         return time.format(formatter);
     }
 
-    public static String formattedShortTime(LocalTime time)
-    {
+    public static String formattedShortTime(LocalTime time) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         return time.format(formatter);
     }
-    public static String monthYearFromDate(LocalDate date)
-    {
+
+    public static String monthYearFromDate(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
         return date.format(formatter);
     }
 
-    public static String monthDayFromDate(LocalDate date)
-    {
+    public static String monthDayFromDate(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd");
-
-    public static String monthYearFromDate(LocalDate date){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-
         return date.format(formatter);
     }
 
-    public static ArrayList<LocalDate> daysInMonthArray()
-    {
+    public static ArrayList<LocalDate> daysInMonthArray() {
         ArrayList<LocalDate> daysInMonthArray = new ArrayList<>();
         YearMonth yearMonth = YearMonth.from(selectedDate);
         int daysInMonth = yearMonth.lengthOfMonth();
@@ -58,47 +49,35 @@ public class CalendarUtils
         LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
         int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
 
-        for(int i = 1; i <= 52; i++){
-            if( i <= dayOfWeek)
-            {
-                daysInMonthArray.add(LocalDate.of(prevMonth.getYear(), prevMonth.getMonth(),prevDaysInMonth + i - dayOfWeek));
-            }
-            else if (i > daysInMonth + dayOfWeek)
-            {
-                daysInMonthArray.add(LocalDate.of(nextMonth.getYear(), nextMonth.getMonth(),i - dayOfWeek - daysInMonth));
-            }
-            else
-            {
-                daysInMonthArray.add(LocalDate.of(selectedDate.getYear(), selectedDate.getMonth(),i - dayOfWeek));
+        for (int i = 1; i <= 52; i++) {
+            if (i <= dayOfWeek) {
+                daysInMonthArray.add(LocalDate.of(prevMonth.getYear(), prevMonth.getMonth(), prevDaysInMonth + i - dayOfWeek));
+            } else if (i > daysInMonth + dayOfWeek) {
+                daysInMonthArray.add(LocalDate.of(nextMonth.getYear(), nextMonth.getMonth(), i - dayOfWeek - daysInMonth));
+            } else {
+                daysInMonthArray.add(LocalDate.of(selectedDate.getYear(), selectedDate.getMonth(), i - dayOfWeek));
             }
         }
         return daysInMonthArray;
     }
 
-    public static ArrayList<LocalDate> daysInWeekArray(LocalDate selectedDate)
-    {
+    public static ArrayList<LocalDate> daysInWeekArray(LocalDate selectedDate) {
         ArrayList<LocalDate> days = new ArrayList<>();
-        LocalDate current =  sundayForDate(selectedDate);
+        LocalDate current = sundayForDate(selectedDate);
         LocalDate endDate = current.plusWeeks(1);
 
-        while (current.isBefore(endDate))
-        {
+        while (current.isBefore(endDate)) {
             days.add(current);
             current = current.plusDays(1);
         }
-
-
         return days;
     }
 
-    private static LocalDate sundayForDate(LocalDate current)
-    {
+    private static LocalDate sundayForDate(LocalDate current) {
         LocalDate lastWeek = current.minusWeeks(1);
 
-        while (current.isAfter(lastWeek))
-        {
-            if(current.getDayOfWeek() == DayOfWeek.SUNDAY)
-            {
+        while (current.isAfter(lastWeek)) {
+            if (current.getDayOfWeek() == DayOfWeek.SUNDAY) {
                 return current;
             }
             current = current.minusDays(1);
