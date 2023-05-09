4c424fb9afd9632411d2656aafd3148e3de0c175
jhgugvut <Japhet8marvel@gmail.com>
2023-05-09 15:39:10 -0400

    Had to start all over...again. No big deal.

---
+++ app/src/main/java/com/example/qalendar/ColorPicker3.java
@@ -0,0 +1,17 @@
+package com.example.qalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+public class ColorPicker3 extends AppCompatActivity {
+
+    ConstraintLayout mLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_picker3);
+    }
+}
