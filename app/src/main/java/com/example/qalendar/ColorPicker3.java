d8df87cb405dbfac5ea38e88dfac4025f4b60085
jhgugvut <Japhet8marvel@gmail.com>
2023-05-09 20:30:55 -0400

    There was a problem with the constraints, but I fixed those as well.

--- app/src/main/java/com/example/qalendar/ColorPicker3.java
+++ app/src/main/java/com/example/qalendar/ColorPicker3.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -113,5 +114,20 @@ public class ColorPicker3 extends AppCompatActivity {
             }
         });
         colorPicker3.show();
+    }
+
+    public void monthlyAction(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+
+    }
+
+    public void weeklyAction(View view) {
+        startActivity(new Intent(this, WeeklyViewActivity.class));
+
+    }
+
+    public void dailyAction(View view) {
+        startActivity(new Intent(this, DailyCalendarActivity.class));
+
     }
 }
